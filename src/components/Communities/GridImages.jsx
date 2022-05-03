792fa62c78025d1e912a0b2f0999e0153521340f
David Lazaro Fernandez <davidlazaro20@hotmail.com>
2022-05-02 22:44:43 -0500

    Css grid

--- src/components/Communities/GridImages.jsx
+++ src/components/Communities/GridImages.jsx
@@ -10,20 +10,45 @@ const GridImages = () => {
     gap: 3,
     gridTemplateColumns: "repeat(2, 1fr)",
   };
-  const back = {
-    backgroundColor: "green",
-  };
+  const imageStyle = {
+    width: "100%",
+    height:'100%', 
+    objectFit:'cover', 
+    borderRadius:'5px'
+  }
+  const imageStyle2 ={
+    width: "100%",
+    objectFit:'cover', 
+    borderRadius:'5px'
+  }
   const elements = [1, 2, 3, 4];
   return (
     <>
-      <Box sx={wrapper}>
-        {elements.map((element) => {
-          return (
-            <>
-              <div style={back}>{element}</div>
-            </>
-          );
-        })}
+      <Box
+        sx={{
+          display: "grid",
+          gridTemplateColumns: "repeat(4, 1fr)",
+          gridTemplateRows: "auto",
+          gridTemplateAreas: `"Photo1 Photo1 Photo4 Photo4"
+                  "Photo1 Photo1 Photo4 Photo4"
+                  "Photo2 Photo-3 Photo4 Photo4"
+                  "Photo2 Photo-3 Photo4 Photo4"`,
+          gap: 1,
+        }}
+      >
+        <Box sx={{ gridArea: "Photo1",}}>
+          <img src="/image 18.png" style={imageStyle2}/>
+          <img src="/image (9).png" style={imageStyle2}/>
+        </Box>
+        <Box sx={{ gridArea: "Photo2",}}>
+          <img src="/image 14.png" style={imageStyle} />
+        </Box>
+        <Box sx={{ gridArea: "Photo-3", }}>
+          <img src="/image 10.png" style={imageStyle}/>
+        </Box>
+        <Box sx={{ gridArea: "Photo4",  }}>
+          <img src="/Group 18.png" style={imageStyle} />
+        </Box>
       </Box>
     </>
   );
--- src/components/Communities/index.js
+++ src/components/Communities/index.js
@@ -28,37 +28,43 @@ const Communities = () => {
         <img src="/Fluent Emojis/devil.png" style={titleImage} />
       </Box>
       <GridImages />
-      <Typography
-        variant="body"
-        sx={{
-          width: "280px",
-          color: "#838383",
-          fontFamily: "'Inter'",
-          fontSize: "16px",
-          fontWeight: "400"
-        }}
-      >
-        From meeting the one of the founders of GitHub to organizing one of the
-        greatest AI GAMING in Latin America and being top 6 in a world wide
-        Local Hack Day.
-        <br />
-        <br />
-        Communities have been a part of my everyday life throughout my journey
-        as a developer. In them I have been able to find everything, from
-        incredible friends, brothers from another mother, life mentors and
-        people whom I am very proud to admire.
-        <br />
-        <br />
-        Creating a community is not easy and for real trust me you need to put
-        200% of you love and passion to it to create real impact into your
-        community, but what you get thanks to it is completly worth it.
-        <br />
-        <br />
-        Likewise, founding a community has been a game changer for my student
-        life. Making a positive impact on your campus is one of the coolest
-        things you can do during your life as a student. That is why I love
-        being part of the following communities
-      </Typography>
+      <Box sx={{marginTop: "40px",}}>
+        <Typography
+          variant="body"
+          sx={{
+            width: "280px",
+            color: "#838383",
+            fontFamily: "'Inter'",
+            fontSize: "16px",
+            fontWeight: "400",
+            
+          }}
+        >
+          From meeting the one of the founders of GitHub to organizing one of
+          the greatest AI GAMING in Latin America and being top 6 in a world
+          wide Local Hack Day.
+          <br />
+          <br />
+          Communities have been a part of my everyday life throughout my journey
+          as a developer. In them I have been able to find everything, from
+          incredible friends, brothers from another mother, life mentors and
+          people whom I am very proud to admire.
+          <br />
+          <br />
+          Creating a community is not easy and for real trust me you need to put
+          200% of you love and passion to it to create real impact into your
+          community, but what you get thanks to it is completly worth it.
+          <br />
+          <br />
+          Likewise, founding a community has been a game changer for my student
+          life. Making a positive impact on your campus is one of the coolest
+          things you can do during your life as a student. That is why I love
+          being part of the following communities
+        </Typography>
+      </Box>
+      <Box sx={{marginTop:'40px'}}>
+        <img src="/Frame 16.png" width="100%"/>
+      </Box>
     </>
   );
 };
